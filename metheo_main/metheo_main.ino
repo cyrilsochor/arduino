2cdcb655909efe07660d80c1c947e04ae2811bdf
Cyril Sochor <cyril.sochor@gmail.com>
2017-06-29 21:59:52 +0200

    small improvements

--- metheo_main/metheo_main.ino
+++ metheo_main/metheo_main.ino
@@ -1,8 +1,11 @@
 // see https://arduino.cz/tutorial-uzivani-hodin-realneho-casu-ds1307-a-ds3231-s-arduinem/
 
+#include <SPI.h> // required for small oled
 #include "Wire.h" // required for clock module
 #include <OneWire.h> // temp sensor DS18B20
 #include <DallasTemperature.h> // temp sensor DS18B20
+#include <Adafruit_GFX.h> // required for small oled
+#include <Adafruit_SSD1306.h> // required for small oled
 
 #define DS3231_I2C_ADDRESS 0x68 // required for clock module
 
@@ -12,15 +15,29 @@ DallasTemperature senzoryDS(&oneWireDS);
 
 byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
 
+// small oled variables
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 void setup(){
     setupClock();
     setupTemperature();
+    setupSmallOled();
     
     Serial.begin(9600);
     
     // set the initial time here:
     // DS3231     ss,mm,HH,dw,dd,MM,YY
-    //setDS3231time(00,37,18,07,21,01,17);
+    //setDS3231time(00,42,06,04,04,05,17);
 }
 
 void setupClock(){
@@ -30,18 +47,69 @@ void setupClock(){
 void setupTemperature(){
   senzoryDS.begin();
 }
-  
+
+void setupSmallOled(){
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
 void loop(){
+  int val = analogRead(A0);
+  float mv = ( val/1024.0)*5000; 
+  float cel = mv/10;
+  float farh = (cel*9)/5 + 32;
+  
+  Serial.print("TEMPRATURE = ");
+  Serial.print(cel);
+  Serial.print("*C");
+  Serial.println();
 
-  displayTimeSerial(); // display the real-time clock data on the Serial Monitor,
+  //displayTimeSerial(); // display the real-time clock data on the Serial Monitor,
+  //displayTempSerial();
+  
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
-
   senzoryDS.requestTemperatures();
-  Serial.print("Teplota cidla DS18B20: ");
-  Serial.print(senzoryDS.getTempCByIndex(0));
-  Serial.println(" stupnu Celsia");
 
-  delay(1000);
+  display.clearDisplay();
+  displayTimeSmallOled();
+  displayTempSmallOled();
+  display.display();
+ 
+  //delay(1000);
+}
+
+char timestring[3];
+void displayTimeSmallOled(){
+
+  //display hour
+  display.setTextSize(5);
+  display.setCursor(0,0);
+  sprintf(timestring,"%2u", hour);
+  display.print(timestring);
+
+  // display calibration pixel
+  //display.drawPixel(127, 40, WHITE);
+  display.fillCircle(60,10,2, WHITE);
+  display.fillCircle(60,25,2, WHITE);
+
+  // display minute
+  display.setCursor(67,0);
+  display.setTextSize(5);
+  sprintf(timestring,"%.2u", minute);
+  display.print(timestring);
+}
+
+char tempstring[7];
+void displayTempSmallOled(){
+  // display current temp
+  display.setTextSize(2);
+  display.setCursor(00,47);
+  float temp = senzoryDS.getTempCByIndex(0);
+  tempstring[0]=(temp>=0)?'+':'-';
+  dtostrf(temp, 2, 1, tempstring+1);
+  //sprintf(tempstring,"%+2.1f", -12.3);
+  display.print(tempstring);  
 }
 
 void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year){
@@ -122,6 +190,11 @@ void displayTimeSerial(){
     }
 }
 
+void displayTempSerial(){
+  Serial.print("Teplota cidla DS18B20: ");
+  Serial.print(senzoryDS.getTempCByIndex(0));
+  Serial.println(" stupnu Celsia");
+}
 
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val){
