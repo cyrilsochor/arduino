a9f0153152786a646adc7f75f17e9b6f2444113f
Cyril Sochor <cyril.sochor@cgi.com>
2019-11-17 00:09:48 +0100

    version from synobus

--- living_room_power/living_room_power.ino
+++ living_room_power/living_room_power.ino
@@ -1,43 +1,77 @@
 #include <IRremote.h>
 
-int RECV_PIN = 11; // vstup pro IR cidlo
 int POWER_LED_PIN = 2;
-unsigned long REMOTE_POWER_VALUE = 1906441864;
-int DELAY_AFTER_SWITCH = 1000;
+int POWER_RELE_PIN = 3;
+int RECV_PIN = 11; // vstup pro IR cidlo
+unsigned long REMOTE_POWER_CODES[] = {2148500519, 2148467751, // stribrne ovladani, cerveny vlevo nahore
+                                      3772790473 // male cerne ovladani, cervene A
+                                      }; 
+int REMOTE_POWER_CODES_COUNT = sizeof(REMOTE_POWER_CODES)/sizeof(long);
+
+int DELAY_AFTER_SWITCH = 500;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-boolean power = true;
+boolean power = false;
 
 void setup()
 {
+  Serial.println("setup()");
   pinMode(POWER_LED_PIN, OUTPUT);
   switchPowerLed();
+
+  pinMode(POWER_RELE_PIN, OUTPUT);
+  digitalWrite(POWER_RELE_PIN, HIGH); // nejde proud
+
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop() {
-  
+
   if (irrecv.decode(&results)) {
     Serial.println(results.value);
-    if(results.value == REMOTE_POWER_VALUE){
-      Serial.println("SWITCH");
-      power = ! power;
-      switchPowerLed();      
-      delay(DELAY_AFTER_SWITCH);      
-      Serial.println("AFTER DELAY");
+
+    for (int i = 0; i < REMOTE_POWER_CODES_COUNT; i++) {
+      if (results.value == REMOTE_POWER_CODES[i]) {
+        Serial.println("SWITCH");
+
+        power = ! power;
+        switchPowerLed();
+        /* tento tanec se delal pro cerneho psa, ktery se ovladal cernym dratovy ovladanim
+        if (power) {
+          digitalWrite(POWER_RELE_PIN, LOW);// jde proud
+          delay(60);
+          digitalWrite(POWER_RELE_PIN, HIGH);// nejde proud
+          delay(60);
+          digitalWrite(POWER_RELE_PIN, LOW);// jde proud
+        } else {
+          digitalWrite(POWER_RELE_PIN, HIGH);// nejde proud
+        }
+        */
+
+        if (power) {
+          digitalWrite(POWER_RELE_PIN, LOW);// jde proud
+        } else {
+          digitalWrite(POWER_RELE_PIN, HIGH);// nejde proud
+        }
+
+        delay(DELAY_AFTER_SWITCH);
+        
+        Serial.println("AFTER DELAY");
+        break;
+      }
     }
-    
+
     irrecv.resume(); // Receive the next value
   }
   delay(10);
 }
 
 
-void switchPowerLed(){
-  if(power){
+void switchPowerLed() {
+  if (power) {
     digitalWrite(POWER_LED_PIN, HIGH);
   } else {
     digitalWrite(POWER_LED_PIN, LOW);
